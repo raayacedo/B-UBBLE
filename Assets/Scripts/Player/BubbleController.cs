0ffef65f6ef3338351ae62279a09d7fcc0508b94
raayacedo <raayacedo@gmail.com>
2025-01-24 12:22:55 -0400

    WIP Controlador de la burbuja
    *se desarrollo un control para la burbuja desde dentro
    *se agregaron materiales de fisica para rebote de burbuja y piso resbaloso

---
+++ Assets/Scripts/Player/BubbleController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BubbleController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody bubbleRigidbody;
+    [SerializeField]
+    private float pushForce = 5f;
+    [SerializeField]
+    private float edgeThreshold = 0.1f;
+    [SerializeField]
+    private float gravityForce = 9.8f;
+
+    private float bubbleRadius;
+    private Transform[] handTransforms;
+    private Dictionary<Transform, bool> handInContact;
+
+    void Start()
+    {
+        bubbleRadius = GetComponent<SphereCollider>().radius * transform.localScale.x;
+
+        GameObject[] hands = GameObject.FindGameObjectsWithTag("Hand");
+        handTransforms = new Transform[hands.Length];
+        handInContact = new Dictionary<Transform, bool>();
+
+        for (int i = 0; i < hands.Length; i++)
+        {
+            handTransforms[i] = hands[i].transform;
+            handInContact[handTransforms[i]] = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 downwardForce = Vector3.up * gravityForce;
+        bubbleRigidbody.AddForce(downwardForce, ForceMode.Acceleration);
+
+        foreach (Transform hand in handTransforms)
+        {
+            Vector3 handPosition = hand.position;
+            float distanceFromCenter = Vector3.Distance(handPosition, transform.position);
+
+            if (distanceFromCenter >= bubbleRadius - edgeThreshold)
+            {
+                if (!handInContact[hand])
+                {
+                    Vector3 pushDirection = (handPosition - transform.position).normalized;
+                    bubbleRigidbody.AddForce(pushDirection * pushForce, ForceMode.Impulse);
+                    handInContact[hand] = true;
+                }
+            }
+            else
+            {
+                handInContact[hand] = false;
+            }
+        }
+    }
+}
