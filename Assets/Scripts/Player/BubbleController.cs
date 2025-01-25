3b2388001ac68468382578e91ac8343050100fc6
raayacedo <raayacedo@gmail.com>
2025-01-24 23:40:57 -0400

    Mejoras al controller
    *implementacion de sistema de salud, daño y vida

--- Assets/Scripts/Player/BubbleController.cs
+++ Assets/Scripts/Player/BubbleController.cs
@@ -5,21 +5,42 @@ namespace Realyteam.Player
 {
     public class BubbleController : MonoBehaviour
     {
+        [Header("General Settings")]
         [SerializeField]
         private Rigidbody bubbleRigidbody;
         [SerializeField]
         private float pushForce = 5f;
         [SerializeField]
         private float edgeThreshold = 0.1f;
+
+        [Header("Gravity Settings")]
         [SerializeField]
         private float gravityForce = 9.8f;
+
+        [Header("Health Settings")]
+        [SerializeField]
+        private float maxHealth = 100f;
+        [SerializeField]
+        private float healthDecreaseRate = 1f; 
+        [SerializeField]
+        private float airTimeDamageRate = 5f;
+        [SerializeField]
+        private float airTimeMultiplier = 10f; 
+
+        private float currentHealth;
+        private float timeInAir;
+        private bool isGrounded;
 
         private float bubbleRadius;
         private Transform[] handTransforms;
         private Dictionary<Transform, bool> handInContact;
 
+        public float CurrentHealt => currentHealth;
+
+        #region Unity Callbacks
         private void Start()
         {
+            currentHealth = maxHealth;
             bubbleRadius = GetComponent<SphereCollider>().radius * transform.localScale.x;
 
             GameObject[] hands = GameObject.FindGameObjectsWithTag("Hand");
@@ -57,6 +78,75 @@ namespace Realyteam.Player
                     handInContact[hand] = false;
                 }
             }
+
+            UpdateAirTime();
+            DecreaseHealthOverTime();
         }
+        #endregion
+
+        #region Private Methods
+
+        private void UpdateAirTime()
+        {
+            if (IsGrounded())
+            {
+                if (!isGrounded && timeInAir > 0)
+                {
+                    // Calculate damage based on time in air
+                    float landingDamage = timeInAir * airTimeMultiplier;
+                    TakeDamage(landingDamage);
+
+                    // Reset air time
+                    timeInAir = 0f;
+                }
+
+                isGrounded = true;
+            }
+            else
+            {
+                timeInAir += Time.deltaTime;
+                isGrounded = false;
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, bubbleRadius + 0.1f);
+        }
+
+        private void DecreaseHealthOverTime()
+        {
+            TakeDamage(healthDecreaseRate * Time.deltaTime);
+        }
+
+        private void TakeDamage(float damage)
+        {
+            currentHealth -= damage;
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                OnBubbleDestroyed();
+            }
+        }
+
+        private void OnBubbleDestroyed()
+        {
+            Debug.Log("Bubble destroyed!");
+            Destroy(gameObject);
+        }
+
+        #endregion
+
+        #region Public Methods
+        public void Heal(float amount)
+        {
+            currentHealth += amount;
+            if (currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+        }
+        #endregion
     }
 }
